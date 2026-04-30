ddfe317f444fc4b1e3c3846986f45cff78d61b95
Alket Avmeti <aa32508@seeu.edu.mk>
2026-04-30 09:50:50 +0200

    Exam complete

---
+++ components inside scr/app.jsx
@@ -0,0 +1,30 @@
+var photos; //student:Alket Avmeti | Id: 132508.
+ useEffect(() => {
+    import("https://jsonplaceholder.typicode.com/photos")
+     saved: false;
+    category: "nature";
+}, []);
+ let photos[
+{
+    id: number,
+    albumId: number,
+    title: string,
+    url: string,
+    thumbnailUrl:string,
+    saved: boolean,
+    category: string
+}
+]
+if(saved===true){
+    print("photo saved")
+}
+if(category=== "architecture")
+{
+    print("architecture")
+    if(category==="people"){
+        print("people")
+    }else{
+        print("nature")
+    }
+}
+function addPhoto():
